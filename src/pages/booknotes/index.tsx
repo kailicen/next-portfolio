a1068c545d2211d7c939e9a5c903dd15acbf1fb0
kailicen <kailicen226@gmail.com>
2023-04-13 21:45:03 +1000

    booknote page

--- src/pages/booknotes/index.tsx
+++ src/pages/booknotes/index.tsx
@@ -3,42 +3,66 @@ import axios from "axios";
 import BookNoteItem from "@/components/BookNotes/BookNoteItem";
 import PageContent from "@/components/Layout/PageContent";
 import RightContent from "@/components/Layout/RightContent";
-import { Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import BookNoteLoader from "@/components/BookNotes/BookNoteLoader";
 
 const BookNotes: React.FC = () => {
   const [bookNotes, setBookNotes] = useState<[]>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoaded(true);
         const res = await axios.get(
-          "https://kailisblog.com/wp-json/wp/v2/booknotes"
+          `https://kailisblog.com/wp-json/wp/v2/booknotes?page=${page}`
         );
         setBookNotes(res.data);
-        setIsLoaded(true);
+        setTotalPages(parseInt(res.headers["x-wp-totalpages"]));
       } catch (err) {
         console.log(`error: ${err}`);
       }
     };
     fetchData();
-  }, []);
+    window.scrollTo(0, 0); // scroll to top when page changes
+  }, [page]);
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const handlePrevPage = () => {
+    setPage(page - 1);
+  };
 
   return (
     <>
       <PageContent>
         <>
           {isLoaded ? (
-            <Stack
-              mt={{ base: "30px", md: "42px" }}
-              pt={{ base: "30px", md: "42px" }}
-              spacing={3}
-            >
-              {bookNotes.map((booknote, index) => (
-                <BookNoteItem key={index} booknote={booknote} />
-              ))}
-            </Stack>
+            <>
+              <Stack
+                mt={{ base: "30px", md: "42px" }}
+                pt={{ base: "30px", md: "42px" }}
+                spacing={3}
+              >
+                {bookNotes.map((booknote, index) => (
+                  <BookNoteItem key={index} booknote={booknote} />
+                ))}
+              </Stack>
+              {totalPages > 1 && (
+                <Flex justifyContent="space-between" m={4}>
+                  {page > 1 && (
+                    <Button onClick={handlePrevPage}>Previous</Button>
+                  )}
+                  {page < totalPages && (
+                    <Button onClick={handleNextPage}>Next</Button>
+                  )}
+                </Flex>
+              )}
+            </>
           ) : (
             <BookNoteLoader />
           )}
